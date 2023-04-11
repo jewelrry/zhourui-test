f2ebfb927a3600664357de3102a5b794a2986703
zhourui <2228097305@qq.com>
2023-04-11 10:33:13 +0800

    test

--- test.go
+++ test.go
@@ -2,12 +2,21 @@ package zhourui_test
 
 import (
 	"fmt"
-	"strings"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func GetAnStr() string {
-	str1 := "a,b,c"
-	arr1 := strings.Split(str1, ",")
-	fmt.Println(arr1)
-	return "dddd"
+func GetStr() string {
+	dsn := "root:1astWeekend@tcp(127.0.0.1)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	mysqlConfig := mysql.Config{
+		DSN:               dsn, // DSN data source name
+		DefaultStringSize: 191, // string 类型字段的默认长度
+	}
+
+	db, err := gorm.Open(mysql.New(mysqlConfig))
+	if err != nil {
+		fmt.Printf("err:%+v \n", err)
+	}
+	fmt.Println(db)
+	return "aaa"
 }
